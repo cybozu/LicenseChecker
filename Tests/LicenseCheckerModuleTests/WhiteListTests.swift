ad7ad984601abf2e129902ca64ffcfdc32963ad4
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-09-28 11:35:19 +0900

    Added WhiteListTests

--- Tests/LicenseCheckerModuleTests/WhiteListTests.swift
+++ Tests/LicenseCheckerModuleTests/WhiteListTests.swift
@@ -1,8 +1,52 @@
-//
-//  File.swift
-//  
-//
-//  Created by ky0me22 on 2022/09/27.
-//
+import XCTest
+import TestResources
+@testable import LicenseCheckerModule
 
-import Foundation
+final class WhiteListTests: XCTestCase {
+    let testResources = TestResources()
+
+    func test_load_broken_white_list() throws {
+        let jsonPath = "SourcePackagesWhiteListBroken/white-list"
+        let jsonURL = try XCTUnwrap(testResources.getJsonUrl(jsonPath))
+
+        let sut = WhiteList.load(url: jsonURL)
+        XCTAssertNil(sut)
+    }
+
+
+    func test_load_empty_white_list() throws {
+        let jsonPath = "SourcePackagesUnknown/white-list"
+        let jsonURL = try XCTUnwrap(testResources.getJsonUrl(jsonPath))
+
+        let sut = WhiteList.load(url: jsonURL)
+        XCTAssertNotNil(sut)
+
+        let actual = try XCTUnwrap(sut)
+        XCTAssertTrue(actual.licenses.isEmpty)
+        XCTAssertTrue(actual.libraries.isEmpty)
+    }
+
+    func test_load_licenses() throws {
+        let jsonPath = "SourcePackagesApache/white-list"
+        let jsonURL = try XCTUnwrap(testResources.getJsonUrl(jsonPath))
+
+        let sut = WhiteList.load(url: jsonURL)
+        XCTAssertNotNil(sut)
+
+        let actual = try XCTUnwrap(sut)
+        XCTAssertEqual(actual.licenses, ["Apache"])
+        XCTAssertTrue(actual.libraries.isEmpty)
+    }
+
+    func test_load_libraries() throws {
+        let jsonPath = "SourcePackagesSomePackage/white-list"
+        let jsonURL = try XCTUnwrap(testResources.getJsonUrl(jsonPath))
+
+        let sut = WhiteList.load(url: jsonURL)
+        XCTAssertNotNil(sut)
+
+        let actual = try XCTUnwrap(sut)
+        XCTAssertTrue(actual.licenses.isEmpty)
+        XCTAssertEqual(actual.libraries, ["some-package"])
+    }
+}
